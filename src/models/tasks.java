0683d7ec6551aeb782af1477c947ef315ceec71d
yuduru.fukuda <yuzurufukuda2136@gmail.com>
2021-02-28 19:02:16 +0900

    Add database files

---
+++ src/models/tasks.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class tasks {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "createdAt", nullable = false)
+    private Timestamp cratedAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    private Timestamp updatedAt;
+
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCratedAt() {
+        return cratedAt;
+    }
+
+    public void setCratedAt(Timestamp cratedAt) {
+        this.cratedAt = cratedAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+}
